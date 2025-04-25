d3f3e984655bba6554fb38a8cd1a22f554479a33
Adem Dayan <ademdayan36@gmail.com>
2025-04-26 02:54:23 +0300

    Kotlin değişkenler, nesne tabanlı programlama, standart programlama yapısına çalışıldı.

---
+++ app/src/main/java/com/example/myapplication/degiskenler/Odev1.kt
@@ -0,0 +1,141 @@
+package com.example.myapplication.degiskenler
+
+fun main() {
+    var sehir = "Kars"
+    println("Şehir : $sehir")
+
+    var ulke = "Türkiye"
+    println("Ülke : $ulke")
+
+    var telefon = "05321234567"
+    println("Telefon : $telefon")
+
+    var postaKodu = "16100"
+    println("Posta Kodu : $postaKodu")
+
+    var email = "ornek@email.com"
+    println("Email : $email")
+
+    var meslek = "Yazılım Mühendisi"
+    println("Meslek : $meslek")
+
+    var stokMiktari = 150
+    println("Stok Miktarı : $stokMiktari")
+
+    var musteriAdi = "Adem Dayan"
+    println("Müşteri Adı : $musteriAdi")
+
+    var bakiye = 2500.75
+    println("Bakiye : $bakiye")
+
+    var dogumGunu = "15.04.2000"
+    println("Doğum Günü : $dogumGunu")
+
+    var maas = 120000
+    println("Maaş : $maas")
+
+    var medeniDurum = "Bekar"
+    println("Medeni Durum : $medeniDurum")
+
+    var urunYorum = "Ürün çok kaliteli."
+    println("Ürün Yorum : $urunYorum")
+
+    var odemeTarihi = "26.04.2025"
+    println("Ödeme Tarihi : $odemeTarihi")
+
+    var odeme = 500.0
+    println("Ödeme : $odeme")
+
+    var siparisAdeti = 3
+    println("Sipariş Adeti : $siparisAdeti")
+
+    var arabaModeli = "Toyota Corolla"
+    println("Araba Modeli : $arabaModeli")
+
+    var kitapAdi = "Suç ve Ceza"
+    println("Kitap Adı : $kitapAdi")
+
+    var yayinlamaTarihi = "01.01.1866"
+    println("Yayınlama Tarihi : $yayinlamaTarihi")
+
+    var indirimMiktari = 20
+    println("İndirim Miktarı : $indirimMiktari")
+
+    var odaSayisi = 2
+    println("Oda Sayısı : $odaSayisi")
+
+    var enlem = 40.1828
+    println("Enlem : $enlem")
+
+    var boylam = 29.0665
+    println("Boylam : $boylam")
+
+    var urunAdi = "Dizüstü Bilgisayar"
+    println("Ürün Adı : $urunAdi")
+
+    var yemekFiyati = 150.0
+    println("Yemek Fiyatı : $yemekFiyati")
+
+    var marka = "Samsung"
+    println("Marka : $marka")
+
+    var muzikAdi = "Shape of You"
+    println("Müzik Adı : $muzikAdi")
+
+    var videoSuresi = "3:45"
+    println("Video Süresi : $videoSuresi")
+
+    var urunPuani = 4.8
+    println("Ürün Puanı : $urunPuani")
+
+    var resimAdi = "manzara.jpg"
+    println("Resim Adı : $resimAdi")
+
+    var dosyaFormati = "JPEG"
+    println("Dosya Formatı : $dosyaFormati")
+
+    var renk = "Mavi"
+    println("Renk : $renk")
+
+    var renkKodu = "#0000FF"
+    println("Renk Kodu : $renkKodu")
+
+    var telefonModeli = "iPhone 14"
+    println("Telefon Modeli : $telefonModeli")
+
+    var ekranBoyutu = 6.1
+    println("Ekran Boyutu : $ekranBoyutu")
+
+    var agirlik = 180
+    println("Ağırlık : $agirlik gram")
+
+    var ulusalGun = "23 Nisan"
+    println("Ulusal Gün : $ulusalGun")
+
+    var tatilGunu = "Cumartesi"
+    println("Tatil Günü : $tatilGunu")
+
+    var rezervasyonTarihi = "10.08.2025"
+    println("Rezervasyon Tarihi : $rezervasyonTarihi")
+
+    var sokakAdi = "Çiçek Sokak"
+    println("Sokak Adı : $sokakAdi")
+
+    var otobusHatti = "14A"
+    println("Otobüs Hattı : $otobusHatti")
+
+    var kalanDakika = 5
+    println("Kalan Dakika : $kalanDakika")
+
+    var takipKodu = "ABC123456789"
+    println("Takip Kodu : $takipKodu")
+
+    var kuponSuresi = "7 gün"
+    println("Kupon Süresi : $kuponSuresi")
+
+    var kuponKodu = "INDIRIM20"
+    println("Kupon Kodu : $kuponKodu")
+
+    var faturaAdresi = "Atatürk Mah. No:10 İstanbul"
+    println("Fatura Adresi : $faturaAdresi")
+}
---
+++ app/src/main/java/com/example/myapplication/nesne_tabanli_programlama/Odev2Main.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.nesne_tabanli_programlama
+
+fun main() {
+    val o2 = Odev2()
+
+
+}
